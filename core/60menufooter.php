f803331b21f32ea7dd566b497de6d0fe27ddfaac
dlunna <dlunna@gmail.com>
2020-04-30 01:39:17 -0400

    Primera version completa del sitio

---
+++ core/60menufooter.php
@@ -0,0 +1,65 @@
+<div class="container">
+  <h1>Nuestra Universidad</h1>
+  <p>Mapa de sitio</p>
+  <br />
+  <div class="row">
+    <div class="col-md-3">
+      <h2>Institución</h2>
+      <hr />
+        <p><a href="http://www.upp.edu.mx/institucion/#historia">Historia</a></p>
+        <p><a href="http://www.upp.edu.mx/institucion/#mision">Misión</a></p>
+        <p><a href="http://www.upp.edu.mx/institucion/#vision">Visión</a></p>
+        <p><a href="http://www.upp.edu.mx/institucion/#rector">Rector</a></p>
+        <p><a href="http://www.upp.edu.mx/institucion/#modelo">Modelo educativo</a></p>
+        <p><a href="http://www.upp.edu.mx/institucion/#nuestros-compromisos">Nuestros compromisos</a></p>
+        <p><a href="http://www.upp.edu.mx/main/#calendario">Calendario Académico</a></p>
+        <p><a href="http://www.upp.edu.mx/becas/">Becas</a></p>
+        <p><a href="http://www.upp.edu.mx/normatividad/">Normatividad</a></p>
+        <p><a href="https://sii.upp.edu.mx">Sistema de gestión de la calidad</a></p>
+        <p><a href="http://www.upp.edu.mx/transparencia/">Transparencia</a></p>
+        <p><a href="https://drive.google.com/drive/folders/0B1hmxg8QtxfYM3pIOHhodXNCWWs">Indicadores de desempeño</a></p>
+        <p><a href="http://www.upp.edu.mx/rfront/?page_id=834">Ubicación</a></p>
+    </div>
+    <div class="col-md-2">
+      <h2>Legales</h2>
+      <hr />
+        <p><a href="https://drive.google.com/drive/folders/1XLo_jUrY-m3sNkt6stJqW1EQdhIV_-p7">Auditoria externa</a></p>
+        <p><a href="http://www.upp.edu.mx/inventario/">Inventario</a><br /></p>
+        <p><a href="http://www.upp.edu.mx/contraloriasocial/">Contraloría social</a></p>
+        <p><a href="http://www.upp.edu.mx/fondosextraordinarios/">Fondos extraordinarios</a></p>
+        <p><a href="http://www.upp.edu.mx/leygralcontabilidad/">Ley general de contabilidad gubernamental</a></p>
+        <p><a href="http://www.upp.edu.mx/compras">Licitaciones e invitaciones</a></p>
+        <p><a href="http://www.upp.edu.mx/indicadorestacticos">Indicadores tácticos</a></p>
+    </div>
+    <div class="col-md-2">
+      <h2>Centros</h2>
+      <hr />
+        <p><a href="http://www.upp.edu.mx/biblioteca/">Investigación bibliográfica</a></p>
+        <p><a href="http://www.upp.edu.mx/ceetupp/">Equinoterapia CEETUPP</a></p>
+        <p><a href="http://www.upp.edu.mx/curei/">Rehabilitación física</a></p>
+        <p><a href="../lib18/proximamente.php">Centro de lenguas</a></p>
+    </div>
+
+    <div class="col-md-3">
+      <h3>Departamentos</h3>
+      <hr />
+        <p><a href="http://www.upp.edu.mx/internacionalizacion/">Internacionalización</a></p>
+        <p><a href="../lib18/proximamente.php">Cultura</a></p>
+        <p><a href="http://www.upp.edu.mx/atencion_y_promocion_de_la_salud/">Atención y promoción a la salud</a></p>
+        <p><a href="http://site.upp.edu.mx">TIC's</a></p>
+    </div>
+    <div class="col-md-2">
+      <h2>Oferta educativa</h2>
+      <hr />
+        <p><a href="http://www.upp.edu.mx/ofertaeducativa/">Licenciaturas</a></p>
+        <p><a href="http://www.upp.edu.mx/ofertaeducativa/">Ingenierías</a></p>
+        <p><a href="http://www.upp.edu.mx/posgrado/#oferta">Especialidades</a></p>
+        <p><a href="http://www.upp.edu.mx/posgrado/#oferta">Maestrías</a></p>
+        <p><a href="http://www.upp.edu.mx/posgrado/#oferta">Doctorados</a></p>
+    </div>
+
+  </div>
+
+</div> <!-- /container -->
+
+<br />
---
+++ hgo/header-links.js
@@ -0,0 +1,68 @@
+window.IndexMenu = {
+	principal: [
+		{
+			url: "http://gobierno.hidalgo.gob.mx/",
+			titulo: "Gobierno",
+			subpaginas: [
+				{ "titulo": "Gobernador", "url": "http://gobierno.hidalgo.gob.mx/gobernador" },
+				{ "titulo": "Agenda del Gobernador", "url": "http://gobierno.hidalgo.gob.mx/Agenda" },
+				{ "titulo": "Gabinete", "url": "http://gobierno.hidalgo.gob.mx/gabinete" },
+				{ "titulo": "Informes de Gobierno", "url": "http://gobierno.hidalgo.gob.mx/informes" },
+				{ "titulo": "Organismos Descentralizados", "url": "http://gobierno.hidalgo.gob.mx/OrgDescentralizados" },
+				{ "titulo": "Dependencias", "url": "http://gobierno.hidalgo.gob.mx/dependencias" },
+				{ "titulo": "Ex-Gobernadores", "url": "http://gobierno.hidalgo.gob.mx/exgobernadores" },
+				{ "titulo": "Dependencias Federales", "url": "http://gobierno.hidalgo.gob.mx/dependenciasfederales" },
+				{ "titulo": "Aviso de Privacidad", "url": "http://gobierno.hidalgo.gob.mx/AvisoPrivacidad" },
+				{ "titulo": "Teléfonos de Emergencias", "url": "http://gobierno.hidalgo.gob.mx/Emergencias" }
+			]
+		},
+		{
+			url: "http://ruts.hidalgo.gob.mx/",
+			titulo: "Trámites y Servicios",
+			subpaginas: [
+				{
+					url: "https://ruts.hidalgo.gob.mx/?nivel=Transaccional",
+					titulo: "Hacer en Línea"
+				},
+				{
+					url: "https://ruts.hidalgo.gob.mx/?nivel=De inicio",
+					titulo: "Iniciar en Línea"
+				},
+				{
+					url: "https://ruts.hidalgo.gob.mx/?nivel=Informativos",
+					titulo: "Presenciales"
+				}
+			]
+		},
+		{
+			url: "http://hidalgo.travel",
+			titulo: "Hidalgo Travel",
+			subpaginas: [
+				{ "url": "http://hidalgo.travel/turismo-de-naturaleza-hidalgo", "titulo": "Turismo de Naturaleza" },
+				{ "url": "http://hidalgo.travel/turismo-de-naturaleza-hidalgo/#grutas-de-xoxafi", "titulo": "Grutas de Xoxafi" },
+				{ "url": "http://hidalgo.travel/turismo-de-naturaleza-hidalgo/#hgo-adventures", "titulo": "HGO-Adventures" },
+				{ "url": "http://hidalgo.travel/turismo-de-naturaleza-hidalgo/#gotcha", "titulo": "Gotcha" },
+				{ "url": "http://hidalgo.travel/turismo-de-naturaleza-hidalgo/#motocross", "titulo": "Motocross" },
+				{ "url": "http://hidalgo.travel/turismo-de-naturaleza-hidalgo/#automovilismo", "titulo": "Automovilismo" },
+				{ "url": "http://hidalgo.travel/turismo-de-naturaleza-hidalgo/#desarrollos-ecoturisticos", "titulo": "Desarrollos Ecoturísticos" },
+				{ "url": "http://hidalgo.travel/turismo-de-naturaleza-hidalgo/#Ecoalberto", "titulo": "Ecoalberto" },
+				{ "url": "http://hidalgo.travel/turismo-de-naturaleza-hidalgo/#enduro", "titulo": "Enduro" },
+				{ "url": "http://hidalgo.travel/turismo-de-naturaleza-hidalgo/#ciclismo-de-montana", "titulo": "Ciclismo De Montaña" },
+				{ "url": "http://hidalgo.travel/turismo-de-naturaleza-hidalgo/#practica-de-tirolesa", "titulo": " Práctica de Tirolesa" }
+			]
+		},
+		{
+			url: "http://transparencia.hidalgo.gob.mx/",
+			titulo: "Transparencia",
+			subpaginas: [
+				{ "url": "http://transparencia.hidalgo.gob.mx/pag/48Rubros.html", "titulo": "Art. 69 Ley de Transparencia" },
+				{ "url": "https://consultapublicamx.inai.org.mx/vut-web/?idSujetoObigadoParametro=2131&amp;idEntidadParametro=13&amp;idSectorParametro=21", "titulo": "Plataforma Nacional de Transparencia" },
+				{ "url": "http://s-finanzas.hidalgo.gob.mx/pag/transparenciaFinanciera.html", "titulo": "Transparencia Financiera" },
+				{ "url": "http://transparencia.hidalgo.gob.mx/pag/proactiva.html", "titulo": "Transparencia Proactiva" },
+				{ "url": "http://transparencia.hidalgo.gob.mx/pag/acc_imfo.html", "titulo": "Acceso a la Información" },
+				{ "url": "http://transparencia.hidalgo.gob.mx/pag/historica21.html", "titulo": "Histórica (Art. 22)" },
+				{ "url": "http://transparencia.hidalgo.gob.mx/pag/part_ciudadana.html", "titulo": "Participación Ciudadana" }
+			]
+		}
+	]
+};
