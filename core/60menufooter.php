ba38e382e269a012fe8c812d97d7518bee32d729
dlunna <dlunna@gmail.com>
2020-05-12 19:03:29 -0500

    actualizacion de menu footer

--- core/60menufooter.php
+++ core/60menufooter.php
@@ -14,30 +14,31 @@
         <p><a href="http://www.upp.edu.mx/institucion/#nuestros-compromisos">Nuestros compromisos</a></p>
         <p><a href="http://www.upp.edu.mx/main/#calendario">Calendario Académico</a></p>
         <p><a href="http://www.upp.edu.mx/becas/">Becas</a></p>
-        <p><a href="http://www.upp.edu.mx/normatividad/">Normatividad</a></p>
         <p><a href="https://sii.upp.edu.mx">Sistema de gestión de la calidad</a></p>
         <p><a href="http://www.upp.edu.mx/transparencia/">Transparencia</a></p>
         <p><a href="https://drive.google.com/drive/folders/0B1hmxg8QtxfYM3pIOHhodXNCWWs">Indicadores de desempeño</a></p>
-        <p><a href="http://www.upp.edu.mx/rfront/?page_id=834">Ubicación</a></p>
+        <p><a href="http://www.upp.edu.mx/institucion/#ubicacion">Ubicación</a></p>
     </div>
     <div class="col-md-2">
       <h2>Legales</h2>
       <hr />
         <p><a href="https://drive.google.com/drive/folders/1XLo_jUrY-m3sNkt6stJqW1EQdhIV_-p7">Auditoria externa</a></p>
-        <p><a href="http://www.upp.edu.mx/inventario/">Inventario</a><br /></p>
         <p><a href="http://www.upp.edu.mx/contraloriasocial/">Contraloría social</a></p>
         <p><a href="http://www.upp.edu.mx/fondosextraordinarios/">Fondos extraordinarios</a></p>
+        <p><a href="http://www.upp.edu.mx/indicadorestacticos">Indicadores tácticos</a></p>
+        <p><a href="http://www.upp.edu.mx/inventario/">Inventario</a><br /></p>
         <p><a href="http://www.upp.edu.mx/leygralcontabilidad/">Ley general de contabilidad gubernamental</a></p>
         <p><a href="http://www.upp.edu.mx/compras">Licitaciones e invitaciones</a></p>
-        <p><a href="http://www.upp.edu.mx/indicadorestacticos">Indicadores tácticos</a></p>
+        <p><a href="http://www.upp.edu.mx/normatividad/">Normatividad</a></p>
     </div>
     <div class="col-md-2">
       <h2>Centros</h2>
       <hr />
-        <p><a href="http://www.upp.edu.mx/biblioteca/">Investigación bibliográfica</a></p>
+        <p><a href="http://www.upp.edu.mx/cal/">Centro de lenguas</a></p>
         <p><a href="http://www.upp.edu.mx/ceetupp/">Equinoterapia CEETUPP</a></p>
+        <p><a href="http://www.upp.edu.mx/biblioteca/">Investigación bibliográfica</a></p>
         <p><a href="http://www.upp.edu.mx/curei/">Rehabilitación física</a></p>
-        <p><a href="../lib18/proximamente.php">Centro de lenguas</a></p>
+
     </div>
 
     <div class="col-md-3">
