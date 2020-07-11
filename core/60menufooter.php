6db43043eccd02282eaa04c6296bef8f8e69b9a7
dlunna <dlunna@gmail.com>
2020-07-10 20:28:37 -0500

    Extensión

--- core/60menufooter.php
+++ core/60menufooter.php
@@ -10,47 +10,54 @@
         <p><a href="http://www.upp.edu.mx/institucion/#mision">Misión</a></p>
         <p><a href="http://www.upp.edu.mx/institucion/#vision">Visión</a></p>
         <p><a href="http://www.upp.edu.mx/institucion/#rector">Rector</a></p>
-        <p><a href="http://www.upp.edu.mx/institucion/#modelo">Modelo educativo</a></p>
-        <p><a href="http://www.upp.edu.mx/institucion/#nuestros-compromisos">Nuestros compromisos</a></p>
+        <p><a href="http://www.upp.edu.mx/institucion/#modelo">Modelo Educativo</a></p>
+        <p><a href="http://www.upp.edu.mx/institucion/#nuestros-compromisos">Nuestros Compromisos</a></p>
         <p><a href="http://www.upp.edu.mx/main/#calendario">Calendario Académico</a></p>
         <p><a href="http://www.upp.edu.mx/becas/">Becas</a></p>
-        <p><a href="https://sii.upp.edu.mx">Sistema de gestión de la calidad</a></p>
+        <p><a href="https://sii.upp.edu.mx">Sistema de Gestión de la Calidad</a></p>
         <p><a href="http://www.upp.edu.mx/transparencia/">Transparencia</a></p>
-        <p><a href="https://drive.google.com/drive/folders/0B1hmxg8QtxfYM3pIOHhodXNCWWs">Indicadores de desempeño</a></p>
+        <p><a href="https://drive.google.com/drive/folders/0B1hmxg8QtxfYM3pIOHhodXNCWWs">Indicadores de Desempeño</a></p>
         <p><a href="http://www.upp.edu.mx/institucion/#ubicacion">Ubicación</a></p>
     </div>
     <div class="col-md-2">
       <h2>Legales</h2>
       <hr />
-        <p><a href="https://drive.google.com/drive/folders/1XLo_jUrY-m3sNkt6stJqW1EQdhIV_-p7">Auditoria externa</a></p>
-        <p><a href="http://www.upp.edu.mx/contraloriasocial/">Contraloría social</a></p>
-        <p><a href="http://www.upp.edu.mx/fondosextraordinarios/">Fondos extraordinarios</a></p>
-        <p><a href="http://www.upp.edu.mx/indicadorestacticos">Indicadores tácticos</a></p>
+        <p><a href="http://www.upp.edu.mx/auditoriaexterna/">Auditoria Externa</a></p>
+        <p><a href="http://www.upp.edu.mx/contraloriasocial/">Contraloría Social</a></p>
+        <p><a href="http://www.upp.edu.mx/fondosextraordinarios/">Fondos Extraordinarios</a></p>
+        <p><a href="http://www.upp.edu.mx/indicadorestacticos">Indicadores Tácticos</a></p>
         <p><a href="http://www.upp.edu.mx/inventario/">Inventario</a><br /></p>
-        <p><a href="http://www.upp.edu.mx/leygralcontabilidad/">Ley general de contabilidad gubernamental</a></p>
-        <p><a href="http://www.upp.edu.mx/compras">Licitaciones e invitaciones</a></p>
+        <p><a href="http://www.upp.edu.mx/leygralcontabilidad/">Ley General de Contabilidad Gubernamental</a></p>
+        <p><a href="http://www.upp.edu.mx/compras">Licitaciones e Invitaciones</a></p>
         <p><a href="http://www.upp.edu.mx/normatividad/">Normatividad</a></p>
     </div>
     <div class="col-md-2">
       <h2>Centros</h2>
       <hr />
-        <p><a href="http://www.upp.edu.mx/cal/">Centro de lenguas</a></p>
+        <p><a href="http://www.upp.edu.mx/cal/">Centro de Lenguas</a></p>
+        <p><a href="http://www.upp.edu.mx/plata/">Polideportivo Plata</a></p>
         <p><a href="http://www.upp.edu.mx/ceetupp/">Equinoterapia CEETUPP</a></p>
-        <p><a href="http://www.upp.edu.mx/biblioteca/">Investigación bibliográfica</a></p>
-        <p><a href="http://www.upp.edu.mx/curei/">Rehabilitación física</a></p>
+        <p><a href="http://www.upp.edu.mx/biblioteca/">Investigación Bibliográfica</a></p>
+        <p><a href="http://www.upp.edu.mx/patentamiento/">Patentamiento</a></p>
+        <p><a href="http://www.upp.edu.mx/curei/">Rehabilitación Física</a></p>
 
     </div>
 
     <div class="col-md-3">
+      <h2>Direcciones</h2>
+      <hr />
+        <p><a href="http://www.upp.edu.mx/vinculacion/">Vinculación y Extensión</a></p>
       <h3>Departamentos</h3>
       <hr />
-        <p><a href="http://www.upp.edu.mx/internacionalizacion/">Internacionalización</a></p>
+        <p><a href="http://www.upp.edu.mx/atencion_y_promocion_de_la_salud/">Atención y Promoción a la Salud</a></p>
         <p><a href="../lib18/proximamente.php">Cultura</a></p>
-        <p><a href="http://www.upp.edu.mx/atencion_y_promocion_de_la_salud/">Atención y promoción a la salud</a></p>
+        <p><a href="http://www.upp.edu.mx/adistancia/">Educación a Distancia</a></p>
+        <p><a href="http://www.upp.edu.mx/internacionalizacion/">Internacionalización</a></p>
+        <p><a href="http://www.upp.edu.mx/serviciosescolares/">Servicios Escolares</a></p>
         <p><a href="http://site.upp.edu.mx">TIC's</a></p>
     </div>
     <div class="col-md-2">
-      <h2>Oferta educativa</h2>
+      <h2>Oferta Educativa</h2>
       <hr />
         <p><a href="http://www.upp.edu.mx/ofertaeducativa/">Licenciaturas</a></p>
         <p><a href="http://www.upp.edu.mx/ofertaeducativa/">Ingenierías</a></p>
