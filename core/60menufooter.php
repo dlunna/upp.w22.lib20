19f645061ea249944140f067d54a61c68af6824e
d4v <dlunna@gmail.com>
2023-04-14 16:50:30 -0600

    indicadores de desempenno

--- core/60menufooter.php
+++ core/60menufooter.php
@@ -17,7 +17,7 @@
         <p><a href="http://www.upp.edu.mx/becas/">Becas</a></p>
         <p><a href="https://sii.upp.edu.mx">Sistema de Gestión de la Calidad</a></p>
         <p><a href="http://www.upp.edu.mx/transparencia/">Transparencia</a></p>
-        <p><a href="http://www.upp.edu.mx/indicadores_de_desempenno/">Indicadores de Desempeño</a></p>
+        <!--<p><a href="http://www.upp.edu.mx/indicadores_de_desempenno/">Indicadores de Desempeño</a></p>-->
         <p><a href="http://www.upp.edu.mx/bancodedatosestadisticos/">Banco de datos estadisticos</a></p>
         <p><a href="http://www.upp.edu.mx/institucion/#ubicacion">Ubicación</a></p>
     </div>
